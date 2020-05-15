37bafa3f19710f732b8fe4ea7f6f561c08dcb80a
TriDvaRas <02dimon02@gmail.com>
2020-05-15 18:16:00 +0300

    Update opban.js

--- commands/CivRandomizer/opban.js
+++ commands/CivRandomizer/opban.js
@@ -1,15 +1,14 @@
 //imports 
 var fs = require('fs');
 var Perm = require('./PermissionsFunctions.js');
-
+var FF = require('./FileFunctions.js');
 module.exports = {
     name: 'opban',
     description: 'Bans Civilization by id or alias ignoring bans limit (OP)',
-    help: '`!civ opban [Id/Alias/skip]`',
+    help: '`!civ opban [Id/Alias]`',
     execute:async function(message, args) {
         //read GameState
-        var data = fs.readFileSync('./commands/CivRandomizer/CurrentState.json', "utf8");
-        var CurrState = JSON.parse(data);
+        var CurrState = FF.Read('./commands/CivRandomizer/CurrentState.json');
         //check phase
         if (CurrState.started != "true") {
             message.channel.send("`!start` game first");
@@ -93,11 +92,7 @@ module.exports = {
 
         }
         //write
-        fs.writeFile('./commands/CivRandomizer/CurrentState.json', JSON.stringify(CurrState, null, 2), function (err) {
-            if (err) {
-                console.log(err);
-            }
-        });
+        FF.Write('./commands/CivRandomizer/CurrentState.json', CurrState);
     },
 };
 //check if civ is banned
@@ -113,5 +108,14 @@ function Ban(C, CurrState) {
 
     CurrState.Civs.splice(CurrState.Civs.findIndex(x => x == C.id), 1);
     CurrState.banned.push(C.id);
+
+}
+Array.prototype.includesIgnoreCase = function (value) {
+    let LCvalue = value.toLowerCase();
+    arr.forEach(E => {
+        if (LCvalue == E.toLowerCase())
+            return true;
+    });
+    return false;
 
 }

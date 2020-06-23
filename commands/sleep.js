ceec9356ac307b11d09a70529876323fb439a7de
TriDvaRas <02dimon02@gmail.com>
2020-06-23 12:17:56 +0300

    added sleep command

---
+++ commands/sleep.js
@@ -0,0 +1,23 @@
+
+const logger = require("../logger");
+const chalk = require("chalk");
+module.exports = {
+	aliases: [`sl'`,`sleep`],
+	description: 'sleep',
+	help: '`!sleep`',
+	execute,
+};
+async function execute(message, args) {
+	if (message.member.voice.channel) {
+		const connection = await message.member.voice.channel.join();
+		logger.log('cmd',`S in ${connection.channel.guild}/${connection.channel.name}`);
+		const dispatcher = connection.play('./commands/assets/s.mp3', {
+			volume: 0.5,
+		});
+		dispatcher.on('finish', () => {
+			logger.log('cmd',`Finished S in ${connection.channel.guild}/${connection.channel.name}`);
+			connection.disconnect();
+			dispatcher.destroy();
+		});
+	}
+}

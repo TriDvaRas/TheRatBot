8d9f87c076c03b1221182aeca8ed90844c896471
TriDvaRas <02dimon02@gmail.com>
2020-08-21 16:53:24 +0300

    muteday fix

--- functions/voiceHandler.js
+++ functions/voiceHandler.js
@@ -15,15 +15,16 @@ function checkMuteDay(oldState, newState) {
     if (lastDay == currDay) return;
     lastDay = currDay;
 
-    let memberCount = newState.channel.members.reduce((accumulator, member) => accumulator + 1, 0)
+    let memberCount = newState.channel.members.size
     if (memberCount < 3) return;
-
     if (!newState.channel.members.every(member => member.voice.selfMute)) return;
+    logger.log(`info`, `Starting mute day count down`);
 
 
     setTimeout(() => {
+        logger.log(`info`, `Checking if still muted`);
         newState.channel.fetch().then(channel => {
-            if (channel.members.reduce((accumulator, member) => accumulator + 1, 0) < 3) return;
+            if (channel.members.size < 3) return;
             if (!channel.members.every(member => member.voice.selfMute)) return;
             channel.guild.channels.cache.array().find(channel => channel.name == `main`).send(
                 new Discord.MessageEmbed()
@@ -32,7 +33,10 @@ function checkMuteDay(oldState, newState) {
                     .setColor('#00dbb7')
                     .setTimestamp()
                     .setFooter('<-- KPblCA', 'https://tdr.s-ul.eu/hP8HuUCR')
-            )
+            ).then(() => {
+
+                logger.log(`info`, `Sent mute day msg`);
+            })
             appendSheet(channel.members.reduce((accumulator, member) => accumulator + `${member.user.tag}\n`, ``))
         })
     }, 60000)
@@ -48,11 +52,11 @@ function appendSheet(users) {
             const sheet = doc.sheetsByIndex[2];
             logger.log(`info`, `Fetched sheet ${sheet.title} ${sheet.columnCount}:${sheet.rowCount}`);
             let date = new Date();
-            sheet.addRow({  
-                date: `${date.getDay()}.${date.getMonth()+1}.${date.getFullYear()}`,
+            sheet.addRow({
+                date: `${date.getDay()}.${date.getMonth() + 1}.${date.getFullYear()}`,
                 uyoubkees: users
             }).then(row => {
-                logger.log(`cmd`, `Added muteDay `)
+                logger.log(`sheet`, `Added muteDay `)
             })
         });
     });
